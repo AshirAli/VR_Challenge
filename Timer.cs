dac73b442700162d0de8de46beb44c6a53e6a91f
alimrafeek <40698894+alimrafeek@users.noreply.github.com>
2018-07-04 23:08:58 +0530

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	Text timerText;
+	public int timeRemaining=10;
+
+	void Start () {
+
+		timerText = GetComponent<Text> ();
+		StartCoroutine (StartTimer ());
+
+	}
+	
+	IEnumerator StartTimer () {
+		Debug.Log ("Started");
+			
+		while (true) {
+			timerText.text = "Time Remaining : " + timeRemaining;
+			yield return new WaitForSeconds (1);
+			timeRemaining--;
+
+			if (timeRemaining < 0) {
+
+				Debug.Log ("Timer Stopped.");
+				StopCoroutine (StartTimer ());
+				timeRemaining = 4;
+				SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+
+			}
+			}
+	}
+}
