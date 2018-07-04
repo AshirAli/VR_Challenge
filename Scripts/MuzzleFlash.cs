2de9da8d2271e573a2354e73867c15299ec5aab7
Ashir Ali <32892597+AshirAli@users.noreply.github.com>
2018-07-04 16:49:55 +0530

    Add files via upload

---
+++ Scripts/MuzzleFlash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour {
+
+    public GameObject openFire;
+    public bool IsActive = false;
+    public float sec = 1f;
+    void Start()
+    {
+        StartCoroutine(OpenFire(sec));
+    }
+
+    void StopFlames()
+    {
+        StopCoroutine("OpenFire");
+        IsActive = false; 
+        openFire.SetActive(false);
+    }
+
+    IEnumerator OpenFire(float waitTime)
+    {
+        IsActive = true; 
+        while (true)
+        {
+            openFire.SetActive(true);
+            yield return new WaitForSeconds(waitTime);
+            openFire.SetActive(false);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
