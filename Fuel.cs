e4d381cd87a5b7cbf0a50634fead20187711e6e5
alimrafeek <40698894+alimrafeek@users.noreply.github.com>
2018-07-05 07:14:37 +0530

    Add files via upload

---
+++ Fuel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuel : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+
+		Destroy (gameObject);
+	}
+
+
+
+}
