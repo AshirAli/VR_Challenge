422a6524201099adc648cda8f10149af06007e9c
Ashir Ali <32892597+AshirAli@users.noreply.github.com>
2018-07-06 14:11:03 +0530

    Update Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -1,37 +1,57 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour {
+public class TimerScript : MonoBehaviour
+{
 
-	Text timerText;
-	public static int timeRemaining=10;
+    public Text timerText;
+    public int timeRemaining = 60;
 
-	void Start () {
+    void Start()
+    {
 
-		timerText = GetComponent<Text> ();
-		StartCoroutine (StartTimer ());
+        timerText = GetComponent<Text>();
+        StartCoroutine(StartTimer());
 
-	}
-	
-	IEnumerator StartTimer () {
-		Debug.Log ("Started");
-			
-		while (true) {
-			timerText.text = "Time Remaining : " + timeRemaining;
-			yield return new WaitForSeconds (1);
-			timeRemaining--;
+    }
 
-			if (timeRemaining <= 0) {
 
-				Debug.Log ("Timer Stopped.");
-				StopCoroutine (StartTimer ());
-				timeRemaining = 4;
-				SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+    public IEnumerator StartTimer()
+    {
+        Debug.Log("Started");
+        int temp, min, sec;
+        while (true)
+        {
+            temp = timeRemaining;
+            min = 0; sec = 0;
+            while (temp >= 0)
+            {
+                if (temp >= 60)
+                {
+                    min += 1;
+                    temp -= 60;
+                }
+                else
+                {
+                    sec = temp;
+                    temp -= 60;
+                }
+            }
+            timerText.text = min.ToString("D2") + ":" + sec.ToString("D2");
+            yield return new WaitForSeconds(1);
+            timeRemaining--;
 
-			}
-			}
-	}
+            if (timeRemaining <= 0)
+            {
+
+                Debug.Log("Timer Stopped.");
+                StopCoroutine(StartTimer());
+                //SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+
+            }
+        }
+    }
 }
